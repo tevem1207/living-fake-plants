11cd6c739f22f15d382375fc4b5493278e6fb513
Taehee_Hwang <tevem1207@gmail.com>
2023-05-24 09:17:23 +0900

    Delete api

--- client/src/hooks/usePot.tsx
+++ client/src/hooks/usePot.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
-import { growPot } from "apis/apis";
 import { Pot } from "utils/types";
 
 const usePot = () => {
   const [pot, setPot] = useState<Pot>();
 
-  useEffect(() => {
-    growPot();
-  }, []);
+  useEffect(() => {}, []);
   return {};
 };
 
